a2c199152358e1cdbdca0aada323719643c028a1
Hiếu Nghĩa <capnguyenhieunghia@gmail.com>
2024-11-11 09:25:09 +0700

    p

---
+++ main.js
@@ -0,0 +1,13 @@
+function getCookie(name) {
+    return document.cookie.split('; ')
+        .reduce((r, v) => {
+            const parts = v.split('=');
+            return parts[0] === name ? decodeURIComponent(parts[1]) : r
+        }, '');
+}
+
+window.onload = function() {
+    const username = getCookie('username');
+    const welcomeMessage = username ?  `Xin chào, ${username}!` : 'Xin chào!';
+    document.getElementById('welcome-message').innerText = welcomeMessage;
+};
