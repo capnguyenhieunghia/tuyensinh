b42816f2a8515ca3f504c1b534e7aa81789bf6a8
Hiếu Nghĩa <capnguyenhieunghia@gmail.com>
2024-11-13 21:40:11 +0700

    p

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-// Changing the style of scroll bar
-// window.onscroll = function() {myFunction()};
 
-// function myFunction() {
-// 	var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-// 	var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-// 	var scrolled = (winScroll / height) * 100;
-// 	document.getElementById("myBar").style.width = scrolled + "%"; 
-// }
 
 function scrollAppear() {
   var introText = document.querySelector('.side-text');
@@ -25,8 +17,6 @@ function scrollAppear() {
 }
 
 window.addEventListener('scroll', scrollAppear);
-
-// For switching between navigation menus in mobile mode
 var i = 2;
 function switchTAB() {
   var x = document.getElementById("list-switch");
@@ -40,33 +30,6 @@ function switchTAB() {
   i++;
 }
 
-// For LOGIN
-var x = document.getElementById("login");
-var y = document.getElementById("register");
-var z = document.getElementById("btn");
-var a = document.getElementById("log");
-var b = document.getElementById("reg");
-var w = document.getElementById("other");
-
-function register() {
-  x.style.left = "-400px";
-  y.style.left = "50px";
-  z.style.left = "110px";
-  w.style.visibility = "hidden";
-  b.style.color = "#fff";
-  a.style.color = "#000";
-}
-
-function login() {
-  x.style.left = "50px";
-  y.style.left = "450px";
-  z.style.left = "0px";
-  w.style.visibility = "visible";
-  a.style.color = "#fff";
-  b.style.color = "#000";
-}
-
-// CheckBox Function
 function goFurther() {
   if (document.getElementById("chkAgree").checked == true) {
     document.getElementById('btnSubmit').style = 'background: linear-gradient(to right, #FA4B37, #DF2771);';
@@ -80,7 +43,6 @@ function google() {
   window.location.assign("https://accounts.google.com/signin/v2/identifier?service=accountsettings&continue=https%3A%2F%2Fmyaccount.google.com%2F%3Futm_source%3Dsign_in_no_continue&csig=AF-SEnbZHbi77CbAiuHE%3A1585466693&flowName=GlifWebSignIn&flowEntry=AddSession", "_blank");
 }
 
-// QUIZ Page
 function quizt(frame) {
   document.getElementById('f1').style = 'display: none;';
   document.getElementById('f2').style = 'display: none;';
