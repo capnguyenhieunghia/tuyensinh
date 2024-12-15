1f37d270d746b188dffc1c3c81ad283ecee0104f
Hiếu Nghĩa <capnguyenhieunghia@gmail.com>
2024-12-15 19:21:51 +0700

    l

--- js/home.js
+++ js/home.js
@@ -12,20 +12,4 @@ function getCookie(name) {
     }, '');
 }
 
-const createSnowflake = () => {
-  const snowflake = document.createElement('div');
-  snowflake.className = 'snowflake';
-  snowflake.textContent = '❄';
-  snowflake.style.left = Math.random() * 100 + 'vw';
-  snowflake.style.fontSize = Math.random() * 1.5 + 0.5 + 'em';
-  snowflake.style.animationDuration = Math.random() * 3 + 2 + 's';
-  snowflake.style.opacity = Math.random();
-  document.body.appendChild(snowflake);
-
-  snowflake.addEventListener('animationend', () => {
-      snowflake.remove();
-  });
-};
-
-setInterval(createSnowflake, 2000);
 
