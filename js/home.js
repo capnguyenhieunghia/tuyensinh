abcadd3cb426672087343b2bb93e0df5189b3557
Hiếu Nghĩa <capnguyenhieunghia@gmail.com>
2024-12-17 16:31:18 +0700

    o

--- js/home.js
+++ js/home.js
@@ -2,6 +2,18 @@ document.addEventListener('DOMContentLoaded', function () {
   const username = getCookie('username');
   const welcomeMessage = username ? `Xin chào: ${username}` : 'Xin chào!';
   document.getElementById('welcome-message').innerText = welcomeMessage;
+
+  // Hiện thông báo nổi
+  const popup = document.getElementById('popup');
+  const overlay = document.getElementById('overlay');
+  popup.style.display = 'block';
+  overlay.style.display = 'block';
+
+  // Đóng popup
+  document.getElementById('close').addEventListener('click', function () {
+    popup.style.display = 'none';
+    overlay.style.display = 'none';
+  });
 });
 
 function getCookie(name) {
@@ -12,3 +24,23 @@ function getCookie(name) {
     }, '');
 }
 
+
+// slide
+let slideIndex = 0;
+const slides = document.querySelector('.slides');
+const totalSlides = document.querySelectorAll('.slide').length;
+
+function showSlides() {
+    slideIndex++;
+    if (slideIndex > Math.floor(totalSlides / 3)) {
+        slideIndex = 0; // Reset to first slide
+    }
+    const offset = -slideIndex * (100 / 3); // Tính toán độ dịch chuyển
+    slides.style.transform = `translateX(${offset}%)`;
+
+    // Thay đổi hình ảnh mỗi 6 giây
+    setTimeout(showSlides, 3000);
+}
+
+// Gọi hàm để bắt đầu slideshow
+showSlides();
