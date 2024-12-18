0c0ea5414351263fdaf5a680a15a664133d91ec9
Hiếu Nghĩa <capnguyenhieunghia@gmail.com>
2024-12-18 16:35:37 +0700

    k

--- js/home.js
+++ js/home.js
@@ -3,17 +3,6 @@ document.addEventListener('DOMContentLoaded', function () {
   const welcomeMessage = username ? `Xin chào: ${username}` : 'Xin chào!';
   document.getElementById('welcome-message').innerText = welcomeMessage;
 
-  // Hiện thông báo nổi
-  const popup = document.getElementById('popup');
-  const overlay = document.getElementById('overlay');
-  popup.style.display = 'block';
-  overlay.style.display = 'block';
-
-  // Đóng popup
-  document.getElementById('close').addEventListener('click', function () {
-    popup.style.display = 'none';
-    overlay.style.display = 'none';
-  });
 });
 
 function getCookie(name) {
@@ -31,15 +20,15 @@ const slides = document.querySelector('.slides');
 const totalSlides = document.querySelectorAll('.slide').length;
 
 function showSlides() {
-    slideIndex++;
-    if (slideIndex > Math.floor(totalSlides / 3)) {
-        slideIndex = 0; // Reset to first slide
-    }
-    const offset = -slideIndex * (100 / 4); // Tính toán độ dịch chuyển
-    slides.style.transform = `translateX(${offset}%)`;
+  slideIndex++;
+  if (slideIndex > Math.floor(totalSlides / 3)) {
+    slideIndex = 0; // Reset to first slide
+  }
+  const offset = -slideIndex * (100 / 2); // Tính toán độ dịch chuyển
+  slides.style.transform = `translateX(${offset}%)`;
 
-    // Thay đổi hình ảnh mỗi 6 giây
-    setTimeout(showSlides, 3000);
+  // Thay đổi hình ảnh mỗi 6 giây
+  setTimeout(showSlides, 3000);
 }
 
 // Gọi hàm để bắt đầu slideshow
