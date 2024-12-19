80ca8abc87208ae9f67fe9472388266b25e500b4
Hiếu Nghĩa <capnguyenhieunghia@gmail.com>
2024-12-19 15:05:07 +0700

    j

--- js/home.js
+++ js/home.js
@@ -12,24 +12,3 @@ function getCookie(name) {
       return parts[0] === name ? decodeURIComponent(parts[1]) : r;
     }, '');
 }
-
-
-// slide
-let slideIndex = 0;
-const slides = document.querySelector('.slides');
-const totalSlides = document.querySelectorAll('.slide').length;
-
-function showSlides() {
-  slideIndex++;
-  if (slideIndex > Math.floor(totalSlides / 3)) {
-    slideIndex = 0; // Reset to first slide
-  }
-  const offset = -slideIndex * (100 / 2); // Tính toán độ dịch chuyển
-  slides.style.transform = `translateX(${offset}%)`;
-
-  // Thay đổi hình ảnh mỗi 6 giây
-  setTimeout(showSlides, 3000);
-}
-
-// Gọi hàm để bắt đầu slideshow
-showSlides();
